6a33443d82a30a2a28e7e7e0883be41c12a596d8
Xin Liu <863320295@qq.com>
2025-05-16 18:17:07 +0800

    Copyright

--- src/Encoder/Encoder.cpp
+++ src/Encoder/Encoder.cpp
@@ -1,13 +1,30 @@
 /* ====================================================================================================================
 
-Copyright(c) 2025, HUAWEI TECHNOLOGIES CO., LTD.
-Licensed under the Code Sharing Policy of the UHD World Association(the "Policy");
-http://www.theuwa.com/UWA_Code_Sharing_Policy.pdf.
-you may not use this file except in compliance with the Policy.
-Unless agreed to in writing, software distributed under the Policy is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OF ANY KIND, either express or implied.
-See the Policy for the specific language governing permissions and
-limitations under the Policy.
+  The copyright in this software is being made available under the License included below.
+  This software may be subject to other third party and contributor rights, including patent rights, and no such
+  rights are granted under this license.
+
+  Copyright (c) 2025, HUAWEI TECHNOLOGIES CO., LTD. All rights reserved.
+  Copyright (c) 2025, PEKING UNIVERSITY. All rights reserved.
+
+  Redistribution and use in source and binary forms, with or without modification, are permitted only for
+  the purpose of developing standards within Audio and Video Coding Standard Workgroup of China (AVS) and for testing and
+  promoting such standards. The following conditions are required to be met:
+
+    * Redistributions of source code must retain the above copyright notice, this list of conditions and
+      the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice, this list of conditions and
+      the following disclaimer in the documentation and/or other materials provided with the distribution.
+    * The name of HUAWEI TECHNOLOGIES CO., LTD. may not be used to endorse or promote products derived from
+      this software without specific prior written permission.
+
+  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES,
+  INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+  ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+  SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+  THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 * ====================================================================================================================
 */
